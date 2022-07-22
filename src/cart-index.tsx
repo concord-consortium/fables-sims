5ac02f0638c53913e0855b47b621ac5fb3537910
Noah Paessel <knowuh@gmail.com>
2022-07-22 11:18:27 -0400

    182141749 - Add shutterbug support.
    [#182141749]
    https://www.pivotaltracker.com/story/show/182141749

--- src/cart-index.tsx
+++ src/cart-index.tsx
@@ -1,9 +1,14 @@
 import * as React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
+import Shutterbug from "shutterbug";
 import { CartApp } from "./components/cart-app";
 
 import "./index.scss";
+
+// Support Shutterbug snapshots.
+Shutterbug.enable();
 
 const rootElement = document.getElementById("app");
 const root = createRoot(rootElement!);
--- src/tugboat-index.tsx
+++ src/tugboat-index.tsx
@@ -2,8 +2,12 @@ import * as React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { TugboatApp } from "./components/tugboat-app";
+import Shutterbug from "shutterbug";
 
 import "./index.scss";
+
+// Support Shutterbug snapshots.
+Shutterbug.enable();
 
 const rootElement = document.getElementById("app");
 const root = createRoot(rootElement!);
